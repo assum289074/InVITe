154ddaffd2c019b6808cd0ed9851149f0ae3a9e3
assum289074 <assumkulumsaani289074@gmail.com>
2024-05-13 12:58:42 +0530

    Delete server directory

--- server/routes/paymentRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { payment } = require("../controllers/paymentController");
-
-router.route("/payment").post(payment);
-module.exports = router;
--- server/routes/userDashboardRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { userDetails } = require("../controllers/userDashboard");
-
-router.route("/details").post(userDetails);
-
-module.exports = router;
